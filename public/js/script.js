634261a92c90675f050ccdfcb3e2502ca143f46b
syahrulfahmi <syahrulfahmi331@gmail.com>
2024-08-11 19:40:12 +0700

    fix: navbar on scroll

--- public/js/script.js
+++ public/js/script.js
@@ -1,19 +1,20 @@
 // NavBar Fixed
 window.onscroll = function () {
-    const header = document.querySelector("header");
-    const fixedNav = header.offsetTop;
+  const header = document.querySelector("header");
+  const fixedNav = header.offsetTop;
 
-    if (window.pageYOffset > fixedNav) {
-        header.classList.add("navbar-fixed");
-    } else {
-        header.classList.remove("navbar-fixed");
-    }
+  console.log(window.scrollY, fixedNav);
+  if (window.scrollY > fixedNav) {
+    header.classList.add("navbar-fixed");
+  } else {
+    header.classList.remove("navbar-fixed");
+  }
 };
 
 // Hamburger Menu
 const hamburger = document.querySelector("#hamburger");
 const navMenu = document.querySelector("#nav-menu");
 hamburger.addEventListener("click", function () {
-    hamburger.classList.toggle("hamburger-active");
-    navMenu.classList.toggle("hidden");
+  hamburger.classList.toggle("hamburger-active");
+  navMenu.classList.toggle("hidden");
 });
