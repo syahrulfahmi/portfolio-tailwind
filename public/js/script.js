3bf4e06043e8c251a5f991abdbadcf1322ca86a0
syahrulfahmi <syahrulfahmi331@gmail.com>
2024-08-11 19:40:58 +0700

    fix: remove console log

--- public/js/script.js
+++ public/js/script.js
@@ -3,7 +3,6 @@ window.onscroll = function () {
   const header = document.querySelector("header");
   const fixedNav = header.offsetTop;
 
-  console.log(window.scrollY, fixedNav);
   if (window.scrollY > fixedNav) {
     header.classList.add("navbar-fixed");
   } else {
